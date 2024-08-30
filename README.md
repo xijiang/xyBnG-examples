23cb42b676805d4b1b04874c9da0f8a703bdf344
Xijiang Yu <xijiang@users.noreply.github.com>
2024-08-30 11:39:38 +0200

    pgsnp
    xyBnG simulation with base population from pgsnp.
    
    Note, if using just one chromosome, there might be singularity error
    soon with GBLUP.  More tests will be performed after EAAP. The current
    version:
    
    - removes all QTL from chip
    - assign random names to alternative SNP alleles
    - some of the parameters were put in the arguments, some are not.
      It doesn't matter very much when using `Revise`.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# xyBnG-drivers
+# Package xyBnG examples
+
+This repository contains examples of the package `xyBnG`. The package `xyBnG` is a Julia package for simulations of complicate breeding schemes. The package xyBnG is available at https://github.com/xijiang/xyBnG.jl.
+
+I haven't register it as a standard Julia package yet. This will be done when I feel comfortable with the package. You can add it by the URL of the package repository. 
+
+This repo, i.e., `xyBnG_examples`, contains examples of using the package `xyBnG`. One need to install the package `xyBnG` before running these examples.
+
+## `pgsnp.jl`
+
+This script read results from `pgsnp.cpp` as founder population and simulate a breeding scheme with the package `xyBnG`. 
+
+### `pgsnp.cpp`
+
+```bash
+g++ -Wall -O3 -std=c++11 -pthread -o pgsnp pgsnp.cpp
+ulimit -s 600000 # increase stack size when necessary
+./pgsnp 200 2000 1.5 1 > snp.txt
+```
+
+This program was written in 2010. It uses too much stack memory. The `ulimit -s 600000` is to increase the stack size.
+
+### `pgsnp.jl`
+
+Warning: This script calls `pgsnp`. You have to make sure that the stack size if large enough. 
+
+```bash
+ulimit -s 600000 # increase stack size when necessary
+julia
+```
+
+You are also recomended to have `Revise` installed. Insert the following code in `~/.julia/config/startup.jl`:
+
+```julia
+using Revise
+```
+
+Above will load `Revise` automatically when you start Julia. In the future, if you `includet("your-script.jl")`, the script will be revised automatically when you save it. You don't have to restart Julia, or `includet` your scripts again.
+
+Run the following code in Julia to us the script `pgsnp.jl`:
+
+```julia
+includet("path-to/pgsnp.jl")
+pgsnp()
+```
---
+++ pgsnp.jl
@@ -0,0 +1,126 @@
+using DataFrames
+using Serialization
+using xyBnG
+import xyBnG.Sum: xysum, savesum, cormat
+import xyBnG.xyTypes: Plan
+import xyBnG.xps: gblup, ablup, iblup, aaocs, iiocs, ggocs, agocs, igocs
+import xyBnG.xps: savepar, initPop
+
+"""
+    pgsnp(; data = "pgsnp", rst = "rst", nrpt = 1)
+# Using `pgsnp` results as base population.
+
+## Manule
+
+Suppose you are in a working directory ``dir``. This file ``pgsnp.jl`` is in
+``script/``. You want to save the results in ``rst``
+
+- Date: 2024-08-27, works ok with xyBnG v1.2.4
+"""
+function pgsnp(;
+    nchp = 8000,
+    nref = 1000,
+    rst = "rst",
+    trait = Trait("growth", 0.1, 1_000),
+    nchr = 2,
+    nrpt = 1,
+)
+    sdir = @__DIR__
+    if !isfile("pgsnp")
+        run(`g++ -Wall -O3 --std=c++11 -pthread -o $sdir/pgsnp $sdir/pgsnp.cpp`)
+    end
+    1 ≤ nchr ≤ 29 || (nchr = 1)
+    species = Cattle(240)
+    nrng, nsel, maf, dF, hist, mr = 5, 20, 0.0, 0.011, 5000, 1.0
+    plan, fixed = Plan(25, 50, 200), ["grt"]
+    CULLS = (gblup, ablup, iblup)
+    OCSS = (aaocs, iiocs, ggocs, agocs, igocs)
+
+    isdir(rst) && rm(rst, force = true, recursive = true)
+    mkpath(rst)
+    scenario = (
+        Result = rst,
+        Species = species,
+        History = hist,
+        MutationRate = mr, # per 1e8 bp per meiosis
+        Trait = trait,
+        Nchr = nchr, # = 1:29
+        Nchp = nchp,
+        Nref = nref,
+        Nrng = nrng,
+        Nsel = nsel,
+        Plan = plan,
+        Fixed = fixed,
+        MAF = maf,
+        ΔF = dF,
+        Nrpt = nrpt,
+        Schemes = union(CULLS, OCSS),
+    )
+    savepar(scenario, "$rst/scenario.par")
+
+    npd = ndigits(nrpt)
+    pln2 = Plan(50, 50, 200)
+    chrln = [
+        1.58,
+        1.36,
+        1.21,
+        1.20,
+        1.18,
+        1.11,
+        1.13,
+        1.05,
+        1.03,
+        1.07,
+        0.87,
+        0.83,
+        0.82,
+        0.85,
+        0.81,
+        0.73,
+        0.66,
+        0.63,
+        0.71,
+        0.69,
+        0.61,
+        0.52,
+        0.62,
+        0.42,
+        0.52,
+        0.46,
+        0.46,
+        0.51,
+    ]
+    open("rst/desc.txt", "w") do io
+        println(io, "BosTau")
+        println(io, species.nid)
+    end
+    for irpt = 1:nrpt
+        tag = lpad(irpt, npd, '0')
+        @info "==========> Repeat: $tag / $nrpt <=========="
+        @info "  - Prepare a founder population"
+        Threads.@threads for i ∈ 1:nchr
+            cmd = `$sdir/pgsnp $(species.nid) $hist $(chrln[i]) $mr`
+            run(pipeline(cmd, stdout = "$rst/chr.$i", stderr = devnull))
+        end
+        xyBnG.Conn.PG.toxy("$rst")
+        fxy = "$rst/$(species.name).xy"
+        fmp = "$rst/$(species.name).lmp"
+        lmp, F0 = initPop(fxy, fmp, rst, plan, maf, nchp, nref, nrng, trait, tag)
+        lmp.chip = lmp.chip .&& .!lmp[!, trait.name] # exclude QTL from chip
+        for scheme in CULLS
+            foo, bar = "$tag-rand", tag * '-' * string(scheme)
+            scheme(rst, foo, bar, lmp, nsel, trait, fixed, plan)
+            summary = xysum("$rst/$bar.ped", "$rst/$bar.xy", lmp, trait)
+            savesum("$rst/summary.ser", summary)
+        end
+        for scheme in OCSS
+            foo, bar = "$tag-rand", tag * '-' * string(scheme)
+            scheme(rst, foo, bar, lmp, nsel, trait, fixed, pln2, dF, F0)
+            summary = xysum("$rst/$bar.ped", "$rst/$bar.xy", lmp, trait)
+            savesum("$rst/summary.ser", summary)
+        end
+    end
+    open("$rst/scenario.par", "a") do io
+        println(io, "Ended: ", time())
+    end
+end
