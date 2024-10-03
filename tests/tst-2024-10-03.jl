a5a1aed69f0d6ad3a4ed552507eaf27bcfd36ab5
Xijiang Yu <xijiang@users.noreply.github.com>
2024-10-03 11:45:59 +0200

    Modification of last commit

--- tests/tst-2024-10-03.jl
+++ tests/tst-2024-10-03.jl
@@ -2,7 +2,7 @@
 This is a test on `2024-10-03`
 
 ## requires
-- ggocs.jl
+- quick-test.jl
 - modified xyBnG.jl v1.3.2 with constraint = 2dF
 
 ## commit
@@ -11,5 +11,5 @@ function devel()
     # 2024-10-03
     # includet("ggocs.jl")
     # commit: 
-    gg_ocs(rst = "quick-test/2df", nrpt = 20, nchp=1100)
+    qtest(rst = "quick-test/2df", nrpt = 20, nchp=1100)
 end
