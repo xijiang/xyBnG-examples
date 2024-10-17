f11c2ea69754932cff8c3a920f40a1d6cba3eb8c
Xijiang Yu <xijiang@users.noreply.github.com>
2024-10-17 17:29:09 +0200

    2024-10-17-1

--- 2024/10/16-1.jl
+++ 2024/10/16-1.jl
@@ -10,26 +10,29 @@ function trial(; nrpt = 100)
     eF = 1 .- (1 - ΔF) .^ (0:ng-1)
     df = DataFrame(Fe = repeat(eF, outer = nrpt))
     Fr, F1d, F2d, F4d, Fb = Float64[], Float64[], Float64[], Float64[], Float64[]
+    @info "Simulation of $nrpt repeats"
     for irpt in 1:nrpt
-        println("==========> Repeat: $irpt / $nrpt <==========")
+        print(" $irpt")
         append!(Fr, random_mate(nsir, ndam, ng))
         append!(F1d, hierarchical_mate(nsir, ndam, noff, ng))
         append!(F2d, hierarchical_mate(nsir, ndam, 2noff, ng))
         append!(F4d, hierarchical_mate(nsir, ndam, 4noff, ng))
         append!(Fb, balanced_mate(nsir, ndam, ng))
     end
+    println()
     df.Fr, df.F1d, df.F2d, df.F4d, df.Fb = Fr, F1d, F2d, F4d, Fb
     serialize("deltaF.ser", df)
 end
 
 function ibdPlot()
     df = deserialize("deltaF.ser")
-    df.grt = repeat(1:25, outer = size(ped, 1) ÷ 25)
+    df.grt = repeat(1:25, outer = size(df, 1) ÷ 25)
     rst = combine(groupby(df, :grt), Not(:grt) .=> mean .=> Not(:grt))
-    plot( rst.grt, rst.Fe, label = "Theoretical")
-    plot!(rst.grt, rst.Fr, label = "Empirical")
-    plot!(rst.grt, rst.F1d, label = "Hierarchical 1d")
-    plot!(rst.grt, rst.F2d, label = "Hierarchical 2d")
-    plot!(rst.grt, rst.F4d, label = "Hierarchical 4d")
-    plot!(rst.grt, rst.Fb, label = "Balanced")
+    fig = plot(rst.grt, rst.Fe, label = "Theoretical")
+    plot!(fig, rst.grt, rst.Fr, label = "Empirical")
+    plot!(fig, rst.grt, rst.F1d, label = "Hierarchical 1d")
+    plot!(fig, rst.grt, rst.F2d, label = "Hierarchical 2d")
+    plot!(fig, rst.grt, rst.F4d, label = "Hierarchical 4d")
+    plot!(fig, rst.grt, rst.Fb, label = "Balanced")
+    fig, rst
 end
--- paper-i.jl
+++ paper-i.jl
@@ -5,37 +5,25 @@ import xyBnG.Sum: xysum, savesum, cormat, savepar
 import xyBnG.xyTypes: Plan
 import xyBnG.xps: initPop, chkbase, ggocs, aaocs, iiocs, igocs
 
-"""
-Results for paper I.
-
-- 1 vs 29 chromosomes
-- pg (ulimit -s 1000000) vs tskit
-- 5 vs 15 generations
-- ΔF = 1.1% 0.5%
-- remove ri-, add ig-, not BLUPs    
-"""
-function paper_1_tk(bdir; nrpt = 100, ε = 1e-6)
+function paper_1_tk(bdir, dF, rst; nrpt = 100, ε = 1e-6)
     plan, plnb, fixed = Plan(25, 50, 200), Plan(50, 50, 200), ["grt"]
-    nrng, nsel, maf, dF = 5, 30, 0.0, 0.011
+    nrng, nsel, maf = 5, 30, 0.0
     species = Cattle(5_000)
     lgnm = 24.89385779     # length of genome in Morgen
-    base = "base/vchr/$cdir"
-    blm = deserialize("$base/BosTau.lmp")
+    blm = deserialize("$bdir/BosTau.lmp")
     lng = combine(groupby(blm, :chr), :pos => maximum => :len)
     rog = sum(lng.len) / lgnm / 1e8 # ratio of genome
     nchp = Int(round(5e4 * rog))
     nref = Int(round(1e4 * rog))
     trait = Trait("growth", 0.25 * rog, nref) # nqtl = nref
     OCSS = (aaocs, ggocs, iiocs, igocs)
-    rst = "rst/vchr/$cdir"
     isdir(rst) && rm(rst, force = true, recursive = true)
     mkpath(rst)
     scenario = (
-        BaseDir = base,
+        BaseDir = bdir,
         Result = rst,
         Species = species,
         Trait = trait,
-        Nchr = cdir,
         Nchp = nchp,
         Nref = nref,
         Nrng = nrng,
@@ -49,17 +37,108 @@ function paper_1_tk(bdir; nrpt = 100, ε = 1e-6)
         Nrpt = nrpt,
     )
     savepar(scenario, "$rst/scenario.par")
-    chkbase(base, species) # Prepare/verify a base population
+    chkbase(bdir, species) # Prepare/verify a base population
     npd = ndigits(nrpt)
     sumfile = "$rst/summary.ser"
-    OCSS = (iiocs, ggocs, riocs)
+    ocss = (iiocs, ggocs, igocs)
+    for irpt ∈ 1:nrpt
+        tag = lpad(irpt, npd, '0')
+        @info "==========> Repeat: $tag / $nrpt <=========="
+        @info "  - Prepare a founder population"
+        fxy = "$base/BosTau.xy"
+        fmp = "$base/BosTau.lmp"
+        lmp, F0 = initPop(fxy, fmp, rst, plan, maf, nchp, nref, nrng, trait, tag)
+        
+        # aaocs
+        foo, bar = "$tag-rand", "$tag-aaocs"
+        aaocs(rst, foo, bar, lmp, nsel, trait, fixed, plnb, dF, F0)
+        summary = xysum("$rst/$bar.ped", "$rst/$bar.xy", lmp, trait)
+        savesum(sumfile, summary)
+
+        for scheme in ocss
+            foo, bar = "$tag-rand", tag * '-' * string(scheme)
+            scheme(rst, foo, bar, lmp, nsel, trait, fixed, plnb, dF, F0; ε = ε)
+            summary = xysum("$rst/$bar.ped", "$rst/$bar.xy", lmp, trait)
+            savesum(sumfile, summary)
+        end
+    end
+    open("$rst/scenario.par", "a") do io
+        println(io, "Ended: ", time())
+    end
 end
 
-function paper_1_pg(bdir; nrpt = 100, ε = 1e-6)
+function paper_1_pg(chr, dF, rst; nrpt = 100)
     # remember `ulimit -s 1000000` before julia REPL
     sdir = @__DIR__
     if !isfile("$sdir/pgsnp")
         @info "Compile pgsnp.cpp"
         run(`g++ -Wall -O3 --std=c++11 -pthread -o $sdir/pgsnp $sdir/pgsnp.cpp`)
+    end
+    nchr = length(chr)
+    ε = nchr < 5 ? 1e-6 : 0.0
+    plan, plnb, fixed = Plan(25, 50, 200), Plan(50, 50, 200), ["grt"]
+    nrng, nsel, maf, hist, mr = 5, 30, 0.0, 5000, 4.0
+    species = Cattle(plan.noff)
+    lgnm = 24.89385779     # length of genome in Morgen
+    rog = sum(chr) / lgnm  # ratio of genome
+    nchp = Int(round(5e4 * rog))
+    nref = Int(round(1e4 * rog))
+    trait = Trait("growth", 0.25 * rog, nref) # nqtl = nref
+    OCSS = (aaocs, iiocs, ggocs, igocs)
+    isdir(rst) && rm(rst, force = true, recursive = true)
+    mkpath(rst)
+    scenario = (
+        Result = rst,
+        Species = species,
+        History = hist,
+        MutationRate = mr, # per 1e8 bp per meiosis
+        Trait = trait,
+        Nchr = nchr,
+        Nchp = nchp,
+        Nref = nref,
+        Nrng = nrng,
+        Nsel = nsel,
+        Plan = plan,
+        Fixed = fixed,
+        MAF = maf,
+        ΔF = dF,
+        Nrpt = nrpt,
+        Schemes = OCSS,
+    )
+    savepar(scenario, "$rst/scenario.par")
+
+    npd = ndigits(nrpt)
+    open("$rst/desc.txt", "w") do io
+        println(io, "BosTau")
+        println(io, species.nid)
+    end
+    for irpt = 1:nrpt
+        tag = lpad(irpt, npd, '0')
+        @info "==========> Repeat: $tag / $nrpt <=========="
+        @info "  - Prepare a founder population"
+        Threads.@threads for i = 1:nchr
+            cmd = `$sdir/pgsnp $(species.nid) $hist $(chr[i]) $mr`
+            run(pipeline(cmd, stdout = "$rst/chr.$i", stderr = devnull))
+        end
+        xyBnG.Conn.PG.toxy("$rst")
+        fxy = "$rst/$(species.name).xy"
+        fmp = "$rst/$(species.name).lmp"
+        lmp, F0 = initPop(fxy, fmp, rst, plan, maf, nchp, nref, nrng, trait, tag)
+
+        # aaocs
+        foo, bar = "$tag-rand", "$tag-aaocs"
+        aaocs(rst, foo, bar, lmp, nsel, trait, fixed, plnb, dF, F0)
+        summary = xysum("$rst/$bar.ped", "$rst/$bar.xy", lmp, trait)
+        savesum(sumfile, summary)
+
+        for scheme in ocss
+            foo, bar = "$tag-rand", tag * '-' * string(scheme)
+            scheme(rst, foo, bar, lmp, nsel, trait, fixed, plnb, dF, F0; ε = ε)
+            summary = xysum("$rst/$bar.ped", "$rst/$bar.xy", lmp, trait)
+            savesum(sumfile, summary)
+        end
+    end
+    open("$rst/scenario.par", "a") do io
+        println(io, "Ended: ", time())
     end
 end
