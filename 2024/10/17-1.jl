b6235c330b541b9bf225b0e1951cfdc24f0b18fe
Xijiang Yu <xijiang@users.noreply.github.com>
2024-10-19 20:08:50 +0200

    lesser repeat

--- 2024/10/17-1.jl
+++ 2024/10/17-1.jl
@@ -45,8 +45,8 @@ function T2024101701(; nrng = 15)
     for i in 1:4
         dF = F[i]
         for chr in (1, 29)
-            paper_1_tk("base/vchr/$chr",  dF, nrng, "rst/tk/c$chr/$i")
-            paper_1_pg(clng[1:$chr], dF, nrng, "rst/pg/c$chr/$i")
+            paper_1_tk("base/vchr/$chr",  dF, nrng, "rst/tk/c$chr/$i"; nrpt = 50)
+            paper_1_pg(clng[1:$chr], dF, nrng, "rst/pg/c$chr/$i"; nrpt = 50)
         end
     end
 end
